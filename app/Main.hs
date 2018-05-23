8a590a4364f8de75c3ce38bcbb8fb908fabb7819
Johannes Hilden <hildenjohannes@gmail.com>
2018-05-23 09:47:25 +0300

    Build out example

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,7 @@
 module Main where
 
 import Foreign.C.String
+import Foreign.Marshal.Alloc
 import Ginac.FFI
 
 main :: IO ()
@@ -8,9 +9,17 @@ main = do
 
     x <- withCString "x" ginac_symbol_new
     p <- ginac_ex_new_from_basic x
+    q <- ginac_ex_new_from_int 1
+    r <- ginac_add_new p q
+    s <- ginac_ex_new_from_basic r
 
-    ginac_ex_print p
+    t <- ginac_ex_to_str s
+    u <- peekCString t
+    print u
+    free t
 
+    ginac_ex_free s
+    ginac_basic_free r
     ginac_ex_free p
     ginac_basic_free x
 
