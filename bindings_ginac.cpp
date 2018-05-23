1ece5bd1cf785226586e86bf84de65feaaba7d5e
Johannes Hilden <hildenjohannes@gmail.com>
2018-05-23 15:11:28 +0300

    Add DEBUG macro

--- bindings_ginac.cpp
+++ bindings_ginac.cpp
@@ -2,41 +2,47 @@
 #include <sstream>
 #include "bindings_ginac.h"
 
+#ifdef DEBUG
+#define DEBUG_PRINT(x) std::cout << "GiNaC::" << x << std::endl
+#else
+#define DEBUG_PRINT(x)
+#endif
+
 using namespace GiNaC;
 
 ex *ginac_ex_new()
 {
-    std::cout << "ex++" << std::endl;
+    DEBUG_PRINT("ex +1");
     return new ex;
 }
 
 ex *ginac_ex_new_from_basic(const basic &other)
 {
-    std::cout << "ex++" << std::endl;
+    DEBUG_PRINT("ex +1");
     return new ex(other);
 }
 
 ex *ginac_ex_new_from_int(int i)
 {
-    std::cout << "ex++" << std::endl;
+    DEBUG_PRINT("ex +1");
     return new ex(i);
 }
 
 ex *ginac_ex_subs(const ex &this_ex, const symbol &s, const ex &other)
 {
-    std::cout << "ex++" << std::endl;
+    DEBUG_PRINT("ex +1");
     return new ex(this_ex.subs(s == other));
 }
 
 ex *ginac_ex_subs_int(const ex &this_ex, const symbol &s, int i)
 {
-    std::cout << "ex++" << std::endl;
+    DEBUG_PRINT("ex +1");
     return new ex(this_ex.subs(s == i));
 }
 
 void ginac_ex_free(ex *this_ex)
 {
-    std::cout << "ex--" << std::endl;
+    DEBUG_PRINT("ex -1");
     delete this_ex;
 }
 
@@ -54,18 +60,18 @@ void ginac_ex_print(const ex &this_ex)
 
 void ginac_basic_free(basic *this_basic)
 {
-    std::cout << "symbol--" << std::endl;
+    DEBUG_PRINT("symbol -1");
     delete this_basic;
 }
 
 symbol *ginac_symbol_new(const char *name)
 {
-    std::cout << "symbol++" << std::endl;
+    DEBUG_PRINT("symbol +1");
     return new symbol(name);
 }
 
 ex *ginac_add(const ex &lh, const ex &rh)
 {
-    std::cout << "ex++" << std::endl;
+    DEBUG_PRINT("ex +1");
     return new ex(add(lh, rh));
 }
