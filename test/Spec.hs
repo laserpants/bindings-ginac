0d190dc9d80f2ff70dd4e5526d5e92f0ace8020a
Johannes Hilden <hildenjohannes@gmail.com>
2018-05-24 22:07:41 +0300

    Add first test

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,19 @@
+import Foreign.C.String
+import Foreign.Marshal.Alloc
+import Math.Ginac.FFI
+import Test.Hspec
+
+testExNew :: IO Int
+testExNew = do
+    ptr <- ginac_ex_new
+    n <- ginac_ex_to_int ptr
+    ginac_ex_free ptr
+    pure n
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $
+
+    describe "ginac_ex_new" $
+      it "should return a GiNaC::ex holding a 0" $ do
+        n <- testExNew
+        n `shouldBe` 0
