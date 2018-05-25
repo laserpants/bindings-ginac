e4d9e0cb0f6bbdf927fc3a529bd4155e1bcd7f09
Johannes Hilden <hildenjohannes@gmail.com>
2018-05-25 13:48:53 +0300

    Update tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,19 +1,32 @@
 import Foreign.C.String
 import Foreign.Marshal.Alloc
+import Foreign.Ptr
 import Math.Ginac.FFI
 import Test.Hspec
 
-testExNew :: IO Int
-testExNew = do
-    ptr <- ginac_ex_new
+extractInt :: Ptr GinacEx -> IO Int
+extractInt ptr = do
     n <- ginac_ex_to_int ptr
     ginac_ex_free ptr
     pure n
 
+testExNew :: IO Int
+testExNew = ginac_ex_new >>= extractInt
+
+testExNewFromInt :: Int -> IO Int
+testExNewFromInt m = ginac_ex_new_from_int m >>= extractInt
+
 main :: IO ()
-main = hspec $
+main = hspec $ do
 
     describe "ginac_ex_new" $
       it "should create a GiNaC::ex object representing 0" $ do
         n <- testExNew
         n `shouldBe` 0
+
+    let m = 5
+
+    describe ("ginac_ex_new_from_int " ++ show m) $
+      it ("should create a GiNaC::ex object == " ++ show m) $ do
+        n <- testExNewFromInt m
+        n `shouldBe` m
