f24f01eb32a570d1437e27a99514dfd63288f6fa
Johannes Hilden <hildenjohannes@gmail.com>
2018-05-26 10:26:41 +0300

    Update Main.hs

--- app/Main.hs
+++ app/Main.hs
@@ -3,10 +3,7 @@ module Main where
 import Foreign
 import Foreign.C.String
 import Foreign.Marshal.Alloc
-import Math.Ginac
 import Math.Ginac.FFI
-import Math.Ginac.FFI.ForeignPtr
-import Math.Ginac.Symbolic
 
 main :: IO ()
 main = pure ()
